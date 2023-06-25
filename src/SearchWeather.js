a438ec699ac071140ea6cd658f0d9a69f426b258
mcecula <mc23051988@gmail.com>
2023-06-25 18:02:21 +0200

    zadanie

---
+++ src/SearchWeather.js
@@ -0,0 +1,13 @@
+const SearchWeather = (props) => {
+    return (
+        <div className="searchWeather">
+            <form>
+                <label htmlFor="searchWeather">Szukaj: </label>
+                <input type="text " id="searchWeather" onChange={(e) => props.filterWeather(e.target.value)} />
+            </form>
+        </div>
+    )
+}
+
+
+export default SearchWeather;
